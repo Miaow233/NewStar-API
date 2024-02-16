4f835c4ea5ccd7146ac5309c0a15bf3f0c2cdfb6
喵呜 <miaow233@qq.com>
2024-02-16 18:40:23 +0800

    Revert "添加API"

--- api/pixel-art/validator.js
+++
@@ -1,8 +0,0 @@
-/**
- * 是否16进制格式颜色
- * @param str
- * @return {boolean}
- */
-export function isColor(str) {
-  return /^#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$/.test(str);
-}
--- scripts/other_code.js
+++
@@ -1,9 +0,0 @@
-function other_code(req, res, next) {
-	res.code = function (code) {
-		res.status(code);
-		res.send();
-	};
-	next();
-}
-
-module.exports = other_code;
