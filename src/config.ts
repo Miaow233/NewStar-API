79775706a2be6eee0ff5d72dec37d15ce68f6c4b
喵呜 <miaow233@qq.com>
2024-02-23 01:18:54 +0800

    feat: 添加默认配置

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import path from 'path'
-
+const { PORT, BAIDU_APP_ID, BAIDU_APP_KEY, PUPPETEER_EXECUTABLE_PATH } = process.env
 const config = {
     ROOT_DIR: __dirname,
-    URL_PORT: 8080,
+    URL_PORT: PORT || 8080,
     URL_PATH: 'http://localhost',
     BASE_VERSION: 'v2',
     CONTROLLER_DIRECTORY: path.join(__dirname, 'controllers'),
@@ -10,8 +10,9 @@ const config = {
     OPENAPI_YAML: '',
     FULL_PATH: '',
     FILE_UPLOAD_PATH: '',
-    BAIDU_APP_ID: '20200403000411421',
-    BAIDU_APP_KEY: 'tb0bCTD9f9_Rq2PKECX8',
+    BAIDU_APP_ID: BAIDU_APP_ID || '20200403000411421',
+    BAIDU_APP_KEY: BAIDU_APP_KEY || 'tb0bCTD9f9_Rq2PKECX8',
+    PUPPETEER_EXECUTABLE_PATH: PUPPETEER_EXECUTABLE_PATH || 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe',
 }
 
 config.OPENAPI_YAML = path.join(config.ROOT_DIR, 'openapi.yaml')
